f0b637f9b7acd304c2f19dd8a5fcddfb10714800
Nikita Khomiuk <nikitakhomiuk@gmail.com>
2023-07-07 20:51:22 +0300

    adding new user handlers and models

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
 import re
 import uuid
+from typing import Optional
 
 from fastapi import HTTPException
-from pydantic import BaseModel, EmailStr, validator
+from pydantic import BaseModel, EmailStr, validator, constr
 
 LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z\-]+$")
 
@@ -30,3 +31,25 @@ class UserCreate(BaseModel):
                 status_code=422, detail="Name should contains only letters"
             )
         return value
+
+
+class DeleteUserResponse(BaseModel):
+    deleted_user_id: uuid.UUID
+
+
+class UpdatedUserResponse(BaseModel):
+    updated_user_id: uuid.UUID
+
+
+class UpdatedUserRequest(BaseModel):
+    name: Optional[constr(min_length=1)]
+    email: Optional[EmailStr]
+
+    @validator("name")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Name should contains only letters"
+            )
+        return value
+
--- db/dals.py
+++ db/dals.py
@@ -32,16 +32,16 @@ class UserDAL:
         if deleted_user_id_row is not False:
             return deleted_user_id_row[0]
 
-    async def get_user_by_id(self, user_id: UUID) -> Optional[UUID]:
+    async def get_user_by_id(self, user_id: UUID) -> Optional[User]:
         query = select(User).where(User.user_id == user_id)
         res = await self.db_session.execute(query)
         user_row = res.fetchone()
         if user_row is not None:
             return user_row[0]
 
-    async def update_user(self, **kwargs) -> Optional[UUID]:
+    async def update_user(self, user_id: UUID, **kwargs) -> Optional[UUID]:
         query = update(User). \
-            where(User.is_active == True). \
+            where(and_(User.user_id == user_id, User.is_active == True)). \
             values(kwargs). \
             returning(User.user_id)
         res = await self.db_session.execute(query)
