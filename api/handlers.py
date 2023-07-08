3dd1bdf20493108f2082772795aad03ad16f4f1b
Nikita Khomiuk <nikitakhomiuk@gmail.com>
2023-07-08 15:45:46 +0300

    modifying handlers

--- api/handlers.py
+++ api/handlers.py
@@ -2,12 +2,16 @@ from typing import Optional
 from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.models import UserCreate, ShowUser, DeleteUserResponse, UpdatedUserResponse, UpdatedUserRequest
 from db.dals import UserDAL
 from db.session import get_db
 
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 user_router = APIRouter()
 
@@ -60,7 +64,12 @@ async def _get_user_by_id(user_id: UUID, db: AsyncSession = Depends(get_db)) -> 
 
 @user_router.post("/", response_model=ShowUser)
 async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> ShowUser:
-    return await _create_new_user(body, db)
+    try:
+        return await _create_new_user(body, db)
+    except IntegrityError as err:
+        logger.error(err)
+        raise HTTPException(status_code=503, detail=f"Database error: {err}")
+
 
 
 @user_router.delete("/", response_model=DeleteUserResponse)
@@ -89,5 +98,9 @@ async def update_user_by_id(
     user = await _get_user_by_id(user_id, db)
     if user is None:
         raise HTTPException(status_code=404, detail=f"User with id {user_id} not found.")
-    updated_user_id = await _update_user(user_id=user_id, updated_user_params=updated_user_params, db=db)
+    try:
+        updated_user_id = await _update_user(user_id=user_id, updated_user_params=updated_user_params, db=db)
+    except IntegrityError as err:
+        logger.error(err)
+        raise HTTPException(status_code=503, detail=f"Database error: {err}")
     return UpdatedUserResponse(updated_user_id=updated_user_id)
