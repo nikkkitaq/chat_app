9f89f7d7504c374e4156de1b3a499e9469283841
Nikita Khomiuk <nikitakhomiuk@gmail.com>
2023-07-09 15:44:02 +0300

    add get_user_by_email method

--- db/dals.py
+++ db/dals.py
@@ -38,6 +38,13 @@ class UserDAL:
         if user_row is not None:
             return user_row[0]
 
+    async def get_user_by_email(self, email: str) -> Optional[User]:
+        query = select(User).where(User.email == email)
+        res = await self.db_session.execute(query)
+        user_row = res.fetchone()
+        if user_row is not None:
+            return user_row[0]
+
     async def update_user(self, user_id: UUID, **kwargs) -> Optional[UUID]:
         query = (
             update(User)
