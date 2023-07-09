941dbfa90078d84b839988f44806c8e63c670ceb
Nikita Khomiuk <nikitakhomiuk@gmail.com>
2023-07-09 15:59:41 +0300

    hashing and security logic

--- settings.py
+++ settings.py
@@ -13,3 +13,7 @@ TEST_DATABASE_URL = env.str(
     "TEST_DATABASE_URL",
     default="postgresql+asyncpg://postgres_test:postgres_test@0.0.0.0:54321/chat_app_test",
 )
+
+SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
+ALGORITHM: str = env.str("ALGORITHM", default="HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
